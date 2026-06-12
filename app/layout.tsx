65850fc0f5dd0c53a42515948671e207c8e32fe7
Alex <petropoulosalex@gmail.com>
2026-06-12 18:54:39 -0400

    fix: added fixed navbar gapping

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.className} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full h-full flex flex-col">
         <Navbar />
-        {children}
+        <div className="h-24" />
+        <main className="grow">{children}</main>
       </body>
     </html>
   );
